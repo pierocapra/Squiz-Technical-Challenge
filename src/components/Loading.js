a80af02746458b8036bec19ac3324c73b2186188
Piero Capra <petecapra07@gmail.com>
2021-11-28 17:34:50 +0000

    companies container re-styled

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div class="d-flex justify-content-center">
+    <div className="d-flex justify-content-center">
       <div className="spinner-border" role="status">
         <span className="visually-hidden">Loading...</span>
       </div>
