416ef9f3c5314d6e209c6946a00ba3000b123e78
Piero Capra <petecapra07@gmail.com>
2021-11-30 15:10:43 +0000

    Title change

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import React from "react";
 const Title = () => {
   return (
     <div className="title">
-      <h2>Technical challenge</h2>
+      <h2>Company Search Engine</h2>
       <div className="underline"></div>
     </div>
   );
